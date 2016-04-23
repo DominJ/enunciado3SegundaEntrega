8af1deefe9190225f7627db00b732bce782e0387
gnizamaeb2desuI <polcosta555@gmail.com>
2016-04-23 16:55:06 +0200

    Update LeerFichero.java

--- clasesCompartidasPrivada/src/clasesCompartidas/LeerFichero.java
+++ clasesCompartidasPrivada/src/clasesCompartidas/LeerFichero.java
@@ -13,18 +13,18 @@ import java.util.HashMap;
 public class LeerFichero 
 {
 	
-	//Si tipusFitxer = 1, utilitzarem el tipusNode per saber la dada primitiva (0 = P, 1 = A, 2 = C, 3 = T)  
+	//C serveix per discriminar el tipus de relaciÃ³ amb paper, utilitzarem la c de la segÃ¼ent manera (1 = PA, 2 = PC, 3 = PT)  
 	
-	//La string archivo hay que escribirla con las barras no invertidas, sino te salta error
-	//IMPORTANTE, devolver un pair con dos hashmaps: HashMap<Integer,ArrayList<Integer>>
-	// HashMap<Integer,ArrayList<Integer>>
+	//Aquest metode retorna un pair amb un HashMap original de relacions a la primera posiciÃ³ i un HashMap transposat a
+	// la segona posiciÃ³ de la relaciÃ³ de dos tipus de node
 	public static Pair<HashMap<Integer,ArrayList<Integer>>,HashMap<Integer,ArrayList<Integer>>> crear_relacion(int c) throws IOException {
 		String cadena;
-		//Este metodo lee el archivo
+		//Aquest metode llegeix el fitxer
+		Integer vb = 0;
 		String archivo = null;
-		if (c == 1) archivo = "/home2/users/alumnes/1193773/dades/DBLP_four_area/paper_author.txt";
-		if (c == 2) archivo = "/home2/users/alumnes/1193773/dades/DBLP_four_area/paper_conf.txt";
-		if (c == 3) archivo = "/home2/users/alumnes/1193773/dades/DBLP_four_area/paper_term.txt";
+		if (c == 1) archivo = "C:/Users/USUARIO/Documents/PROP 2a/Set1/paper_author.txt";
+		else if (c == 2) archivo = "C:/Users/USUARIO/Documents/PROP 2a/Set1/paper_conf.txt";
+		else archivo = "C:/Users/USUARIO/Documents/PROP 2a/Set1/paper_term.txt";
 		FileReader f = new FileReader(archivo); 
 		BufferedReader b = new BufferedReader(f); 
 		HashMap<Integer,ArrayList<Integer>> m = new HashMap<Integer,ArrayList<Integer>>();
@@ -35,20 +35,22 @@ public class LeerFichero
 		String s2 = new String();
 		ArrayList<Integer> l = new ArrayList<Integer>();
 		while(((cadena = b.readLine())!=null)){
+			++vb;
 	    	int i = 0;
 	    	String copy = cadena;
 	    	while(!(Character.isWhitespace(cadena.charAt(i)))) {
 	    		++i;
 	    	}
-	    	
 	    	String s = copy.substring(0,i);
 	    	codi = Integer.parseInt(s);
 	    	String s1 = copy.substring(i+1,copy.length());
+	    	System.out.println("ja hi som " + vb + "\n");
 	    	codi2 = Integer.parseInt(s1);
+	    	System.out.println("ja hi som aqui " + vb + "\n");
 	    	//Segon Hashmap del pair
 	    	if (n.get(codi2) == null) n.put(codi2, new ArrayList<Integer>());
 	    	n.get(codi2).add(codi);
-	    	//Primer Hashmap del pair
+	    	//Primer HashMap del pair
 	    	if(primer){
 	    		s2 = copy.substring(0,i);
 	    		codi_antic = codi;
@@ -63,25 +65,25 @@ public class LeerFichero
 	    	s2 = copy.substring(0,i);
 		}
 		m.put(codi_antic, l);
-		//Cerramos el buffer
+		//Tanquem el buffer
 		b.close();
-		
+		//Omplim el pair amb els dos HashMaps obtinguts
 		Pair<HashMap<Integer,ArrayList<Integer>>,HashMap<Integer,ArrayList<Integer>>> v = new Pair<HashMap<Integer,ArrayList<Integer>>,HashMap<Integer,ArrayList<Integer>>>();
 		v.setFirst(m);
 		v.setSecond(n);
 		return v;
 	}
 
-	//IMPORTANTE, devolver un pair con dos hashmaps: HashMap<Integer,String>
-	// HashMap<String,Integer>
+	//Aquest mÃ¨tode retorna un pair amb un HashMap original de node primitiu a la primera posiciÃ³ i un HashMap transposat a
+	// la segona posiciÃ³. Els codis dels tipus sÃ³n (0 = P, 1 = A, 2 = C, 3 = T) 
 	public static Pair<HashMap<Integer,String>,HashMap<String,Integer>> crear_nodo_primitivo(int c) throws IOException {
 		String cadena;
-		//Este metodo lee el archivo
+		//Aquest mÃ¨tode llegeix el fitxer
 		String archivo = null;
-		if (c == 0) archivo = "C:/Users/USUARIO/Downloads/PROP/DBLP_four_area/paper.txt";
-		else if (c == 1) archivo = "C:/Users/USUARIO/Downloads/PROP/DBLP_four_area/author.txt";
-		else if (c == 2) archivo = "C:/Users/USUARIO/Downloads/PROP/DBLP_four_area/conf.txt";
-		else archivo = "C:/Users/USUARIO/Downloads/PROP/DBLP_four_area/term.txt";
+		if (c == 0) archivo = "C:/Users/USUARIO/Documents/PROP 2a/Set1/paper.txt";
+		else if (c == 1) archivo = "C:/Users/USUARIO/Documents/PROP 2a/Set1/author.txt";
+		else if (c == 2) archivo = "C:/Users/USUARIO/Documents/PROP 2a/Set1/conf.txt";
+		else archivo = "C:/Users/USUARIO/Documents/PROP 2a/Set1/term.txt";
 		FileReader f = new FileReader(archivo); 
 		BufferedReader b = new BufferedReader(f); 
 		HashMap<Integer,String> m = new HashMap<Integer,String>();
@@ -96,35 +98,17 @@ public class LeerFichero
 	    	String s = copy.substring(0,i);
 	    	codi = Integer.parseInt(s);
 	    	String s1 = copy.substring(i+1,copy.length());
+	    	//IntroduÃ¯m els elements al HashMap primer
 	    	m.put(codi, s1);
+	        //IntroduÃ¯m els elements al HashMap segon ja invertits
 	    	n.put(s1,codi);
 		}
-		//Cerramos el buffer
+		//Tanquem el buffer
 		b.close();
+		//Omplim el pair amb els dos HashMaps obtinguts
 		Pair<HashMap<Integer,String>,HashMap<String,Integer>> v = new Pair<HashMap<Integer,String>,HashMap<String,Integer>>();
 		v.setFirst(m);
 		v.setSecond(n);
 		return v;
 	}
-
-	
-	
-	public static void main(String [] args) throws IOException {
-		Pair<HashMap<Integer,String>,HashMap<String,Integer>> z = crear_nodo_primitivo(1);
-		//HashMap<Integer,ArrayList<Integer>> r = crear_relacion(1);
-		HashMap<Integer,String> s = z.getFirst();
-		HashMap<String,Integer> s1 = z.getSecond();
-
-		//ArrayList<Integer> m = new ArrayList<Integer>();
-		String m = s.get(76);
-		System.out.println("Author " + m + "\t");
-		Integer n = s1.get(m);
-		System.out.println("ID " + n + "\n");
-		//m = r.get(7632);
-		 /* for (Integer number : m) {
-			   System.out.println("Number0 = " + number.);
-		} */
-		   
-	}
-
 }
