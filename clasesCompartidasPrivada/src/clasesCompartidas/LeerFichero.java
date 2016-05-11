9c0901fcedc84fe7f68211b65ffe461bed30a64d
gnizamaeb2desuI <polcosta555@gmail.com>
2016-05-11 16:04:56 +0200

    Update LeerFichero.java

--- clasesCompartidasPrivada/src/clasesCompartidas/LeerFichero.java
+++ clasesCompartidasPrivada/src/clasesCompartidas/LeerFichero.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * @author Daniel Pulido Sánchez-Carnerero
+ * @author Daniel Pulido SÃ¡nchez-Carnerero
  *
  */
 public class LeerFichero 
@@ -16,24 +16,24 @@ public class LeerFichero
 	
 	/*PRE: c es un integer que discrimina el tipus*/
 	/*POST: Es crea un pair de dos HashMaps, amb la relacio P-NodePrimitiu al First i NodePrimitiu-P al Second*/
-	public static Pair<HashMap<Integer,ArrayList<Pair<Integer,Double>>>,HashMap<Integer,ArrayList<Pair<Integer,Double>>>> crear_relacion(int c, String ruta) throws IOException {
+	public static Pair<HashMap<Integer,ArrayList<Pair<Integer,Double>>>,HashMap<Integer,ArrayList<Pair<Integer,Double>>>> crear_relacion(int c) throws IOException {
 		String cadena;
+		//Aquest metode llegeix el fitxer
 		Integer vb = 0;
 		String archivo = null;
-		if (c == 4) archivo = ruta;
-		else if (c == 1) archivo = "../Set1/paper_author.txt";
-		else if (c == 2) archivo = "../Set1/paper_conf.txt";
-		else archivo = "../Set1/paper_term.txt";
+		if (c == 1) archivo = "Set1/paper_author.txt";
+		else if (c == 2) archivo = "Set1/paper_conf.txt";
+		else archivo = "Set1/paper_term.txt";
 		FileReader f = new FileReader(archivo); 
 		BufferedReader b = new BufferedReader(f); 
 		HashMap<Integer,ArrayList<Pair<Integer,Double>>> m = new HashMap<Integer,ArrayList<Pair<Integer,Double>>>();
 		HashMap<Integer,ArrayList<Pair<Integer,Double>>> n = new HashMap<Integer,ArrayList<Pair<Integer,Double>>>();
 		Integer codi,codi2,codi_antic;
 		codi = codi2 = codi_antic = 0;
-		Pair<Integer,Double> x = new Pair<Integer,Double>();
-		Pair<Integer,Double> z = new Pair<Integer,Double>();
 		Boolean primer = true;
 		String s2 = new String();
+		Pair<Integer,Double> x = new Pair<Integer,Double>();
+		Pair<Integer,Double> x1 = new Pair<Integer,Double>();
 		ArrayList<Pair<Integer,Double>> l = new ArrayList<Pair<Integer,Double>>();
 		while(((cadena = b.readLine())!=null)){
 			++vb;
@@ -46,10 +46,14 @@ public class LeerFichero
 	    	codi = Integer.parseInt(s);
 	    	String s1 = copy.substring(i+1,copy.length());
 	    	codi2 = Integer.parseInt(s1);
+	    	//Segon Hashmap del pair
 	    	if (n.get(codi2) == null) n.put(codi2, new ArrayList<Pair<Integer,Double>>());
+	    	//n.get(codi2).add(codi);
 	    	x.setFirst(codi);
 	    	x.setSecond(1.0);
 	    	n.get(codi2).add(x);
+	    	x = new Pair<Integer,Double>();
+	    	//Primer HashMap del pair
 	    	if(primer){
 	    		s2 = copy.substring(0,i);
 	    		codi_antic = codi;
@@ -59,65 +63,43 @@ public class LeerFichero
 	    		m.put(codi_antic, l);
 	    		l = new ArrayList<Pair<Integer,Double>>();
 	    	}
-	    	z.setFirst(codi2);
-	    	z.setSecond(1.0);
-	    	l.add(z);
+	    	x1.setFirst(codi2);
+	    	x1.setSecond(1.0);
+	    	l.add(x1);
+	    	x1 = new Pair<Integer,Double>();
 	    	codi_antic = codi;
 	    	s2 = copy.substring(0,i);
 		}
 		m.put(codi_antic, l);
+		//Tanquem el buffer
 		b.close();
+		//Omplim el pair amb els dos HashMaps obtinguts
 		Pair<HashMap<Integer,ArrayList<Pair<Integer,Double>>>,HashMap<Integer,ArrayList<Pair<Integer,Double>>>> v = new Pair<HashMap<Integer,ArrayList<Pair<Integer,Double>>>,HashMap<Integer,ArrayList<Pair<Integer,Double>>>>();
 		v.setFirst(m);
 		v.setSecond(n);
 		return v;
 	}
-
-	//(0 = P, 1 = A, 2 = C, 3 = T) 
-	/*PRE: c es un integer que discrimina el tipus*/
-	/*POST: Es crea un pair de dos HashMaps, amb la relacio dels dos atributs del NodePrimitiu*/
-	public static Pair<HashMap<Integer,String>,HashMap<String,Integer>> crear_nodo_primitivo(int c) throws IOException {
-		String cadena;
-		String archivo = null;
-		if (c == 0) archivo = "../Set1/paper.txt";
-		else if (c == 1) archivo = "../Set1/author.txt";
-		else if (c == 2) archivo = "../Set1/conf.txt";
-		else archivo = "../Set1/term.txt";
-		FileReader f = new FileReader(archivo); 
-		BufferedReader b = new BufferedReader(f); 
-		HashMap<Integer,String> m = new HashMap<Integer,String>();
-		HashMap<String,Integer> n = new HashMap<String,Integer>();
-		Integer codi = 0;
-		while(((cadena = b.readLine())!=null)){
-	    	int i = 0;
-	    	String copy = cadena;
-	    	while(!(Character.isWhitespace(cadena.charAt(i)))) {
-	    		++i;
-	    	}
-	    	String s = copy.substring(0,i);
-	    	codi = Integer.parseInt(s);
-	    	String s1 = copy.substring(i+1,copy.length());
-	    	m.put(codi, s1);
-	    	n.put(s1,codi);
-		}
-		b.close();
-		Pair<HashMap<Integer,String>,HashMap<String,Integer>> v = new Pair<HashMap<Integer,String>,HashMap<String,Integer>>();
-		v.setFirst(m);
-		v.setSecond(n);
-		return v;
-	}
 	
-	public void main() throws IOException{
+	public static void main(String [] args) throws IOException{
 		Pair<HashMap<Integer,ArrayList<Pair<Integer,Double>>>,HashMap<Integer,ArrayList<Pair<Integer,Double>>>> a = new Pair<HashMap<Integer,ArrayList<Pair<Integer,Double>>>,HashMap<Integer,ArrayList<Pair<Integer,Double>>>>();
-		String h = "hahaha";
-		a = crear_relacion(1,h);
-		HashMap<Integer,ArrayList<Pair<Integer,Double>>> b = new HashMap<Integer,ArrayList<Pair<Integer,Double>>>();
-		b = a.getFirst();
-		ArrayList<Pair<Integer,Double>> c = new ArrayList<Pair<Integer,Double>>();
-		c = b.get(1);
-	    for(int v=0;v<c.size();v++) {
-	        System.out.println(c.get(v).getFirst() + " \n");
-	      }
+		a = crear_relacion(1);
+		HashMap<Integer,ArrayList<Pair<Integer,Double>>> b0 = new HashMap<Integer,ArrayList<Pair<Integer,Double>>>();
+		HashMap<Integer,ArrayList<Pair<Integer,Double>>> b1 = new HashMap<Integer,ArrayList<Pair<Integer,Double>>>();
+		b0 = a.getFirst();
+		b1 = a.getSecond();
+		ArrayList<Pair<Integer,Double>> c0 = new ArrayList<Pair<Integer,Double>>();
+		ArrayList<Pair<Integer,Double>> c1 = new ArrayList<Pair<Integer,Double>>();
+		c0 = b0.get(10);
+		//c1 = b1.get(10);
+		System.out.println("Size0 = " + c0.size() + "\n");
+		//System.out.println("Size1 = " + c1.size() + "\n");
+		System.out.println("Number0 = " + c0.get(1).getFirst() + "\n");
+		Pair<Integer,Double> d0 = new Pair<Integer,Double>();
+		Pair<Integer,Double> d1 = new Pair<Integer,Double>();
+		//d0 = c0.get(0);
+		//d1 = c1.get(0);
+		//System.out.println("Number0 = " + d0.getFirst() + "\n");
+		//System.out.println("Number1 = " + d1.getFirst() + "\n");
 	}
 	
 }
