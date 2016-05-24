bd70381c8e47b0ac073ed33de4b13f3eeac48b52
gnizamaeb2desuI <polcosta555@gmail.com>
2016-05-24 12:24:28 +0200

    Update VistaAnadir1.java

--- clasesCompartidasPrivada/src/clasesCompartidas/VistaAnadir1.java
+++ clasesCompartidasPrivada/src/clasesCompartidas/VistaAnadir1.java
@@ -1,167 +1,142 @@
 package clasesCompartidas;
 
-import java.awt.Checkbox;
-import java.awt.CheckboxGroup;
-import java.awt.Component;
-import java.awt.Container;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Label;
-import java.awt.Point;
-import java.awt.TextField;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class VistaMBD {
+	  private CtrlPresentacion iCtrlPresentacion;
+	  private JFrame frameVista1 = new JFrame("Opcions");
+	  private JPanel panelContenidos1 = new JPanel();
+	  private JPanel panelBotones1 = new JPanel();
+	  private JButton buttonA = new JButton("Anadir");
+	  private JButton buttonAR = new JButton("Actualizar");
+	  private JButton buttonLH = new JButton("Limpiar Historial");
+	  private JButton buttonE = new JButton("Eliminar");
+
+	  
+
+public void activar() {
+    frameVista1.setEnabled(true);
+  }
+
+  public void desactivar() {
+    frameVista1.setEnabled(false);
+  }
+	  public void hacerVisible() {
+	    frameVista1.pack();
+	    frameVista1.setVisible(true);
+	  }
+
+	  public void hacerInvisible() {
+	    frameVista1.setVisible(false);
+	  }
+	  
+	  private void inicializar_frameMBD() {
+			System.out.println("No existeix\n");
+			frameVista1.setMinimumSize(new Dimension(530,200));
+		    frameVista1.setPreferredSize(frameVista1.getMinimumSize());
+		    frameVista1.setResizable(false);
+		    // Posicion y operaciones por defecto
+		    frameVista1.setLocationRelativeTo(null);
+		    frameVista1.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		    JPanel contentPanel = (JPanel) frameVista1.getContentPane();
+		    contentPanel.add(panelContenidos1);
+		    JLabel etiqueta1 = new JLabel("MODIFICACIONES");
+		    etiqueta1.setForeground(Color.GREEN);
+		    frameVista1.add(etiqueta1);
+			FlowLayout layout1 = new FlowLayout(FlowLayout.LEFT, 10, 35);
+		    frameVista1.setLayout(layout1);
+		    
+			}
 
 
-public class VistaAnadir1 {
+		private void inicializar_panelContenidos1() {
+		    // Layout
+		    panelContenidos1.setLayout(new BorderLayout());
+		    // Paneles
+		    panelContenidos1.add(panelBotones1,BorderLayout.NORTH);
+		  }
 
-	
-	
-	// Anadir //
-	private static JPanel panelAnadir = new JPanel();
-	private static JPanel panelAnadir1 = new JPanel();;
-	private static JFrame frameVistaAnadir = new JFrame("Añadir");	
-	private static JFrame frameVistaAnadir1 = new JFrame("Añadir DI");
-	private static CheckboxGroup Tipos = new CheckboxGroup();
-	private static Checkbox chkAuthor = new Checkbox("Author",Tipos,false);
-	private static Checkbox chkConf = new Checkbox("Conference",Tipos,false);
-	private static Checkbox chkTerm = new Checkbox("Term",Tipos,false);
-	private static TextField tf1 = new TextField("",20);
-	private static TextField tf2 = new TextField("",20);
-	private static Label PaperL = new Label("Paper");
-	private static JButton buttonOk = new JButton("Ok");
-	
-	
-	private static JButton buttonDI = new JButton("Dato Independiente");
-	private static JButton buttonCD = new JButton("Conjunto de Datos");
-
-	
-	
-	public static void actionPerformed_buttonOk (ActionEvent event) {
-		int a = panelAnadir.getComponentCount();
-		System.out.println("Numberargs:"+a+"\n");
-		//List<JTextField> list = new ArrayLists<JTextField>();
-		Component[] components = panelAnadir.getComponents();
-		String s = "";
-		String n = "";
-		for (Component component : components) {
-		    if (component.getClass().equals(Checkbox.class)) {
-		    	Checkbox b = (Checkbox)component;
-		    	if(b.getState()) {
-		    		s = b.getLabel();
-		    		//System.out.println("Check:"+s+"\n");
-		    	}
-		    }
-		    if (component.getClass().equals(TextField.class)) {
-		    	TextField c = (TextField)component;
-		    	n = c.getText();
-		    }
+		private void inicializar_panelBotones1() {
+		    // Layout
+		    panelBotones1.setLayout(new GridLayout());
+		    // Componentes
+		    panelBotones1.add(buttonA);
+		    panelBotones1.add(buttonAR);
+		    panelBotones1.add(buttonLH);
+		    panelBotones1.add(buttonE);
+		  }
+		
+		
+		public void actionPerformed_buttonA (ActionEvent event) {
+			iCtrlPresentacion.sincronizacionMBD_a_Anadir1();
 		}
-		System.out.println("Check:"+s+"\n");
-		System.out.println("TextField:"+n+"\n");
+		public void asignar_listenersComponentes1() {
 
+		    // Listeners para los botones
+			
+		    buttonA.addActionListener
+		      (new ActionListener() {
+		        public void actionPerformed (ActionEvent event) {
+		          String texto = ((JButton) event.getSource()).getText();
+		          System.out.println("Has clickado el boton con texto: " + texto);
+		          actionPerformed_buttonA(event);
+		        }
+		      });
 
+		    buttonAR.addActionListener
+		      (new ActionListener() {
+		        public void actionPerformed (ActionEvent event) {
+		          String texto = ((JButton) event.getSource()).getText();
+		          System.out.println("Has clickado el boton con texto: " + texto);
+		          actionPerformed_buttonAR(event);
+		        }
+		      });
+
+		    buttonLH.addActionListener
+		      (new ActionListener() {
+		        public void actionPerformed (ActionEvent event) {
+		          String texto = ((JButton) event.getSource()).getText();
+		          System.out.println("Has clickado el boton con texto: " + texto);
+		          actionPerformed_buttonLH(event);
+		        }
+		      });
+		    buttonE.addActionListener
+		    (new ActionListener() {
+		      public void actionPerformed (ActionEvent event) {
+		        String texto = ((JButton) event.getSource()).getText();
+		        System.out.println("Has clickado el boton con texto: " + texto);
+		        VistaEliminar.actionPerformed_buttonE(event);
+		        }
+		    });
+
+		    
+
+		  }
+	  public void inicializarComponentes() {
+		  	inicializar_frameMBD();
+		    inicializar_panelContenidos1();
+		    inicializar_panelBotones1();
+		    asignar_listenersComponentes1();
+		    System.out.println
+		      ("isEventDispatchThread1: " + SwingUtilities.isEventDispatchThread());
+		   hacerVisible();
+		    
+	  }
+	  
+	public VistaMBD(CtrlPresentacion pCtrlPresentacion) {
+	    iCtrlPresentacion = pCtrlPresentacion;
+	    inicializarComponentes();
 	}
-
-private static void asignar_listenersComponentes3() {
-	  buttonOk.addActionListener
-	      (new ActionListener() {
-	        public void actionPerformed (ActionEvent event) {
-	          String texto = ((JButton) event.getSource()).getText();
-	          System.out.println("Has clickado el boton con texto: " + texto);
-	          actionPerformed_buttonOk(event);
-	        }
-	      });
-	}
-
-public static void actionPerformed_buttonCD (ActionEvent event) {
-
-}
-
-public static void actionPerformed_buttonDI (ActionEvent event) {
-	frameVistaAnadir1.setMinimumSize(new Dimension(700,200));
-	frameVistaAnadir1.setResizable(false);
-	frameVistaAnadir1.setLocationRelativeTo(null);
-	frameVistaAnadir1.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-    JPanel contentPaneAnadir = (JPanel) frameVistaAnadir1.getContentPane();
-    contentPaneAnadir.add(panelAnadir1);
-    
-	
-	FlowLayout layout = new FlowLayout(FlowLayout.LEFT, 5, 15);
-	//cambiao ventana -> frameVista
-    frameVistaAnadir1.setLayout(layout);
-    panelAnadir1.setLayout(new FlowLayout());
-	
-    frameVistaAnadir1.setVisible(true);
-    panelAnadir1.setVisible(true);
-    
-    
-    
-    tf1 = new TextField("Anade el valor", 20);
-    tf1.setLocation(30, 30);
-    tf2 = new TextField("Anade el paper", 20);
-    tf2.setLocation(30, 30);
-    panelAnadir1.add(PaperL);
-    panelAnadir1.add(tf2);
-    panelAnadir1.add(chkAuthor);
-    panelAnadir1.add(chkConf);
-    panelAnadir1.add(chkTerm);
-    panelAnadir1.add(tf1);
-    panelAnadir1.add(buttonOk);
-
-
-    
-    //frameVista1.setVisible(false);
-    asignar_listenersComponentes2();
-}
-
-
-
-private static void asignar_listenersComponentes2() {
-	  buttonDI.addActionListener
-	      (new ActionListener() {
-	        public void actionPerformed (ActionEvent event) {
-	          String texto = ((JButton) event.getSource()).getText();
-	          System.out.println("Has clickado el boton con texto: " + texto);
-	          actionPerformed_buttonDI(event);
-	        }
-	      });
-	  buttonCD.addActionListener
-      (new ActionListener() {
-        public void actionPerformed (ActionEvent event) {
-          String texto = ((JButton) event.getSource()).getText();
-          System.out.println("Has clickado el boton con texto: " + texto);
-          actionPerformed_buttonCD(event);
-        }
-      });
-	}
-
-
-
-public static void actionPerformed_buttonA (ActionEvent event) {
-		frameVistaAnadir.setMinimumSize(new Dimension(330,80));
-		frameVistaAnadir.setResizable(false);
-		frameVistaAnadir.setLocationRelativeTo(null);
-		frameVistaAnadir.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-	    JPanel contentPaneAnadir = (JPanel) frameVistaAnadir.getContentPane();
-	    contentPaneAnadir.add(panelAnadir);
-	    
-		
-		FlowLayout layout = new FlowLayout(FlowLayout.LEFT, 5, 15);
-		//cambiao ventana -> frameVista
-	    frameVistaAnadir.setLayout(layout);
-	    panelAnadir.setLayout(new FlowLayout());
-		
-	    frameVistaAnadir.setVisible(true);
-	    panelAnadir.setVisible(true);
-	    
-	    panelAnadir.add(buttonDI);
-	    panelAnadir.add(buttonCD);
-	    asignar_listenersComponentes2();
-	}
-
 }
