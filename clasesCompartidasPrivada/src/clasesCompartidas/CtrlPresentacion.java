c9abce973490311a3d4434d47b6acaa81a821dd0
gnizamaeb2desuI <polcosta555@gmail.com>
2016-05-25 13:47:18 +0200

    Update CtrlPresentacion.java

--- clasesCompartidasPrivada/src/clasesCompartidas/CtrlPresentacion.java
+++ clasesCompartidasPrivada/src/clasesCompartidas/CtrlPresentacion.java
@@ -12,6 +12,10 @@ public class CtrlPresentacion {
 	private vistaDI vistaDI = null;
 	private vistaCD vistaCD = null;
 	private VistaEliminar vistaEliminar = null;
+	private VistaRC vistaRC = null;
+	private VistaCP vistaCP = null;
+	private VistaCCN vistaCCN = null;
+	//private VistaBH vistaBH = null;
 
 	
 
@@ -27,6 +31,77 @@ public class CtrlPresentacion {
 	public void inicializarPresentacion() {
 		//ctrlDominio.inicializarCtrlDominio();
 		vistaPrincipal.hacerVisible();
+	}
+	
+	
+	
+	//----------------------RC-A-CCN--------------------------------------//
+
+	
+	public void sincronizacionRC_a_CCN() {
+		vistaRC.desactivar();
+	    // Solo se crea una vista secundaria (podria crearse una nueva cada vez)
+	    if (vistaCCN == null){
+	    	vistaCCN = new VistaCCN(this);
+	    }
+	    vistaCCN.hacerVisible();
+	  }
+	
+	public void sincronizacionCCN_a_RC1(String a, int b, int c) {
+		sincronizacionCCN_a_RC();	
+	}
+	
+	public void sincronizacionCCN_a_RC() {
+		 vistaCCN.hacerInvisible();
+		 vistaRC.activar();
+		
+	}
+	
+	//----------------------RC-A-CP--------------------------------------//
+
+	
+	public void sincronizacionRC_a_CP() {
+		vistaRC.desactivar();
+	    // Solo se crea una vista secundaria (podria crearse una nueva cada vez)
+	    if (vistaCP == null){
+	    	vistaCP = new VistaCP(this);
+	    }
+	    vistaCP.hacerVisible();
+	  }
+	
+	public void sincronizacionCP_a_RC() {
+		 vistaCP.hacerInvisible();
+		 vistaRC.activar();
+		
+	}
+	
+	//----------------------VistaPrincipal-A-RC--------------------------------------//
+
+	
+	public void sincronizacionVistaPrincipal_a_RC() {
+		vistaPrincipal.desactivar();
+	    // Solo se crea una vista secundaria (podria crearse una nueva cada vez)
+	    if (vistaRC == null){
+	    	vistaRC = new VistaRC(this);
+	    }
+	    vistaRC.hacerVisible();
+	  }
+	
+	public void sincronizacionRC_a_VistaPrincipal() {
+		 vistaRC.hacerInvisible();
+		 vistaPrincipal.activar();
+		
+	}
+	
+	//----------------------VistaPrincipal-A-BH--------------------------------------//
+
+
+	public void sincronizacionVistaPrincipal_a_BH(){
+	  }
+	
+	public void sincronizacionBH_a_VistaPrincipal(int options, String n) {
+		
+		
 	}
 	
 	//----------------------MBD-A-Eliminar--------------------------------------//
