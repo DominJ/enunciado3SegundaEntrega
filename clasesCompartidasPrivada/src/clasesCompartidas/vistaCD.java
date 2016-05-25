87b0e2dd8950e7a8bbaca341a9a94932ebc9d6fd
gnizamaeb2desuI <polcosta555@gmail.com>
2016-05-25 11:50:20 +0200

    Update vistaCD.java

--- clasesCompartidasPrivada/src/clasesCompartidas/vistaCD.java
+++ clasesCompartidasPrivada/src/clasesCompartidas/vistaCD.java
@@ -1,22 +1,104 @@
 package clasesCompartidas;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JOptionPane;
+
 public class vistaCD {
 	  private CtrlPresentacion iCtrlPresentacion;
+	  private JFrame frameVistaCD = new JFrame("Añadir CD");
+	  private JPanel panelCD = new JPanel();
+	  private Label PaperL = new Label("Dialogo");
+	  private JButton buttonAceptar = new JButton("Aceptar");
+	  private JButton buttonCancelar = new JButton("Cancelar");
+
+
+
 
 	public void activar() {
-	    frameVista1.setEnabled(true);
+	    frameVistaCD.setEnabled(true);
 	  }
 
 	  public void desactivar() {
-	    frameVista1.setEnabled(false);
+	    frameVistaCD.setEnabled(false);
 	  }
 		  public void hacerVisible() {
-		    frameVista1.pack();
-		    frameVista1.setVisible(true);
+		    frameVistaCD.pack();
+		    frameVistaCD.setVisible(true);
 		  }
 
 		  public void hacerInvisible() {
-		    frameVista1.setVisible(false);
+		    frameVistaCD.setVisible(false);
+		  }
+		  
+		  
+          void actionPerformed_buttonAceptar(ActionEvent event) {
+        	  //iCtrlPresentacion.actualizarCD();
+          }
+          
+          void actionPerformed_buttonCancelar(ActionEvent event) {
+        	  iCtrlPresentacion.sincronizacionCD_a_Anadir1();
+          }
+
+
+
+private void asignar_listenersComponentes() {
+	  buttonAceptar.addActionListener
+	      (new ActionListener() {
+	        public void actionPerformed (ActionEvent event) {
+	          String texto = ((JButton) event.getSource()).getText();
+	          System.out.println("Has clickado el boton con texto: " + texto);
+	          actionPerformed_buttonAceptar(event);
+	        }
+	      });
+	  buttonCancelar.addActionListener
+      (new ActionListener() {
+        public void actionPerformed (ActionEvent event) {
+          String texto = ((JButton) event.getSource()).getText();
+          System.out.println("Has clickado el boton con texto: " + texto);
+          actionPerformed_buttonCancelar(event);
+        }
+      });
+	}
+
+		  
+		  public void inicializarComponentes () {
+				frameVistaCD.setMinimumSize(new Dimension(800,300));
+				frameVistaCD.setResizable(false);
+				frameVistaCD.setLocationRelativeTo(null);
+				frameVistaCD.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+			    JPanel contentPaneAnadir = (JPanel) frameVistaCD.getContentPane();
+			    contentPaneAnadir.add(panelCD);
+			    
+				
+				FlowLayout layout = new FlowLayout(FlowLayout.LEFT, 5, 15);
+				//cambiao ventana -> frameVista
+			    frameVistaCD.setLayout(layout);
+			    panelCD.setLayout(new FlowLayout());
+				
+			    frameVistaCD.setVisible(true);
+			    panelCD.setVisible(true);
+			    
+			    panelCD.add(PaperL);
+			    PaperL.setText("Para añadir conjuntos de datos, debe añadir los ficheros"
+			    		+ "en la carpeta AddData con los nombres correspondientes,"
+			    		+ "quiere añadir el conjunto de datos?");
+			    panelCD.add(buttonAceptar);
+			    panelCD.add(buttonCancelar);
+			  
+			   
+			    asignar_listenersComponentes(); 
+			 
+
+			 
+		
 		  }
 		  
 		  public vistaCD(CtrlPresentacion pCtrlPresentacion) {
